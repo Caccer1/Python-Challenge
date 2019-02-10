fa65ef3041965c57341e2ef2b3b4adac7e0e3aeb
Jessica C <46266180+Caccer1@users.noreply.github.com>
2019-02-10 13:52:31 -0500

    Add files via upload

---
+++ PyPoll/main.py/PyPoll JC.py
@@ -0,0 +1,23 @@
+import os
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'electiondata.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+#Declare variables
+A = 'Voter ID`
+B = `County`
+C = `Candidate`
+
+#* The total number of votes cast
+
+#* A complete list of candidates who received votes
+
+#* The percentage of votes each candidate won
+
+#* The total number of votes each candidate won
+
+#* The winner of the election based on popular vote.
+
+#* As an example, your analysis should look similar to the one below:
